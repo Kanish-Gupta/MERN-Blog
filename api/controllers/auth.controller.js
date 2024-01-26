90907f532a48c8161640fbb73f3d02329b5363a1
Kanish Gupta <kanish.gupta2021@vitstudent.ac.in>
2024-01-26 11:53:14 +0530

    add delete user api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,6 +3,36 @@ import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
+// export const signup = async (req, res, next) => {
+//   const { username, email, password } = req.body;
+
+//   if (
+//     !username ||
+//     !email ||
+//     !password ||
+//     username === "" ||
+//     email === "" ||
+//     password === ""
+//   ) {
+//     next(errorHandler(400, "All fields are required"));
+//   }
+
+//   const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+
+//   const newUser = new User({
+//     username,
+//     email,
+//     password: hashedPassword,
+//   });
+
+//   try {
+//     await newUser.save();
+//     res.json("Signup successful");
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -17,7 +47,7 @@ export const signup = async (req, res, next) => {
     next(errorHandler(400, "All fields are required"));
   }
 
-  const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
   const newUser = new User({
     username,
